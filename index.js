92c2c7c048ef2c1fefe816585913a4befe0eb880
maw1e <johnmarieygot21@gmail.com>
2024-03-17 19:27:45 +0800

    final app

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-// chrome://extensions/
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLinks = [];
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+// localStorage.clear();
+const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"));
+if (linksFromLocalStorage) {
+  myLinks = linksFromLocalStorage;
+  render(myLinks);
 }
+
+function render(links) {
+  let listItems = "";
+  for (let i = 0; i < links.length; i++) {
+    listItems += `
+              <li>
+                  <a target='_blank' href='${links[i]}'>
+                      ${links[i]}
+                  </a>
+              </li>
+          `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLinks.push(tabs[o].url);
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+    render(myLinks);
+  });
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLinks = [];
+  render(myLinks);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLinks.push(inputEl.value);
+  inputEl.value = "";
+
+  localStorage.setItem("myLinks", JSON.stringify(myLinks));
+
+  console.log(localStorage.getItem("myLinks"));
+
+  render(myLinks);
+});
